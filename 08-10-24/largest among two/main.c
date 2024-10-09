b127cfaadd8a414dda84b45a40a3fbb96d21e4a1
Maxbilal <77446549+Maxbilal@users.noreply.github.com>
2024-10-09 00:22:03 -0700

    Add files via upload

---
+++ 08-10-24/largest among two/main.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a,b,sum;
+    printf("Enter the first digit :");
+    scanf("%d",&a);
+    printf("Enter the second digit :");
+    scanf("%d",&b);
+    sum = a+b;
+    printf("The sum of %d and %d is %d",a,b,sum);
+
+    return 0;
+}
